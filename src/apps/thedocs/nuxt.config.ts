e5e2a76bb32d21d6ceed62eb0e45c2bd2c49dc4e
Sean De Gee <dev@seandegee.tech>
2025-07-29 16:43:39 +0200

    changes

--- src/apps/thedocs/nuxt.config.ts
+++ src/apps/thedocs/nuxt.config.ts
@@ -1,7 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/image'],
   nitro: {
       preset: 'bun',
